23fb12cea739bbb38b1c896c9bfd5875d1b4375f
Sonu445Kum <sonuroy1619@gmail.com>
2025-03-02 13:59:15 +0530

    Project structure set up

---
+++ frontend/src/Components/Signup.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+      <h1>Signup</h1>
+      <form action="">
+        
+      </form>
+    </div>
+  )
+}
+
+export default Signup
